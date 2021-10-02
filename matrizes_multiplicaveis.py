6d92d0b526163214369ab2d1efb0056a9b4718e4
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-10-02 19:19:38 -0300

    Add files via upload

---
+++ matrizes_multiplicaveis.py
@@ -0,0 +1,10 @@
+def sao_multiplicaveis(m1, m2):
+    i1=len(m1)
+    j1=len(m1[0])
+    i2=len(m2)
+    j2=len(m2[0])
+    if j1==i2:
+        return True
+    else:
+        return False
+           
